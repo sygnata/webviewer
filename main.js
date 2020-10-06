17375dcaf796b1dade3c8c8db61bc54879f120f7
Andre <andreluiz_ferraz@hotmail.com>
2020-10-06 20:34:49 -0300

    Toggle Dev Tools

--- main.js
+++ main.js
@@ -2,63 +2,70 @@ const {
   app,
   BrowserWindow,
   globalShortcut
-} = require('electron');
+} = require('electron')
+const config = require('./config')
 
-const config = require('./config.js')
-let win;
+let win = null,
+  contents = null;
 
 function createWindow() {
-  // Cria uma janela de navegação.
+
   win = new BrowserWindow({
     width: config.width,
     height: config.height,
-    titleBarStyle: 'hidden',
     frame: false,
+    titleBarStyle: 'customButtonsOnHover',
     alwaysOnTop: true,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
   })
 
-  // e carrega o arquivo index.html do seu aplicativo.
   win.loadURL(config.url)
+
   contents = win.webContents
-  // Abrir o DevTools (aba de ferramentas para desenvolvedores).
-  contents.openDevTools()
+
+}
+
+function toggleDevTools() {
+  contents.toggleDevTools()
 }
 
 function fullScreen() {
   win.isSimpleFullScreen() ? win.setSimpleFullScreen(false) : win.setSimpleFullScreen(true);
 }
 
-//CTRL + SHIFT + I abre o devTools
 function createShortcuts() {
+  globalShortcut.register('CmdOrCtrl+J', toggleDevTools)
   globalShortcut.register('F11', fullScreen)
 }
-// Este método será chamado quando Electron terminar de inicializar
-// e também estiver pronto para criar novas janelas do navegador.
-// Algumas APIs podem ser usadas somente depois que este evento ocorre.
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.whenReady()
   .then(createWindow)
   .then(createShortcuts)
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed.
 app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-app.on('activate', () => {
+app.on('activate', recreateWindow)
+
+
+function recreateWindow() {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
-})
+}
+app.allowRendererProcessReuse = true;
 
-// Nesse arquivo, você pode incluir o resto do código principal
-// de processos do seu aplicativo.
-// Você também pode colocar eles em arquivos separados e requeridos-as aqui.
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
